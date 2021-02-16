43c1e13d3b16e1a0b3aacbb5e9ebb76da9d11613
Aaron <baokou66@gmail.com>
2021-02-16 15:15:51 -0500

    maded the basic file structure and make a like-terms.py (not yet finish)

---
+++ backend/.history/like_terms_20210216145801.py
@@ -0,0 +1,27 @@
+# 16-2t=5t+9
+class LikeTerm:
+    
+    def __init__(self, equation):
+        self.equation = equation.replace(" ","")
+        self.operators = {
+            "+": " + ",
+            "-": " - ",
+            "*": " * ",
+            "/": " / ",
+            "=", " = "
+        }
+
+    def _splitEquation(self):
+        splitted_equation = self.equation
+        for i in self.operators:
+            splitted_equation.replace(i, self.operators[i])
+        
+        return splitted_equation
+    
+
+    
+
+    
+
+
+    
